2397f57b0deb1d979648a97e96225943c5480aad
Eliud Duno <eliudduno@outlook.com>
2024-08-21 13:05:21 -0400

    agregando variable chart y funcion ue emula el lanzamiento de una moneda,

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
+import scipy.stats
 import streamlit as st 
+import time
 
 st.header('Lanzar una moneda')
+
+chart = st.line_chart([0.5])
+
+def toss_coin(n): # funcion que emula el lanzamiento de una moneda
+    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n) 
+
+    mean = None
+    outcome_no = 0
+    outcome_1_count = 0
+
+    for r in trial_outcomes:
+        outcome_no += 1
+        if r == 1:
+            outcome_1_count += 1
+        mean = outcome_1_count / outcome_no
+        chart.add_rows([mean])
+        time.sleep(0.05)
+    
+    return mean
 
 st.write('Esta aplicación aún no es funcional. En construcción.')
 
